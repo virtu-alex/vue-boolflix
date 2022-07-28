b37abac907d80e2bd05d4a7fc88d201dd72123c1
virtu-alex <alessio.cordari@gmail.com>
2022-07-28 19:38:12 +0200

    moved both API calls into a single function

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,35 @@
   <div id="app">
     <!-- <BaseHeader @search-movies="getQueryMovies" /> -->
     <header>
-      <input type="text" v-model="keyword" />
-      <button @click="getQueryMovies" role="button">Search Movies</button>
-      <button @click="getQuerySeries" role="button">Search Series</button>
+      <div class="container my-5">
+        <input type="text" v-model="keyword" class="me-4" />
+        <button
+          @click="getQuerys"
+          role="button"
+          class="btn btn-primary px-2 me-2"
+        >
+          Search
+        </button>
+      </div>
     </header>
     <main>
-      <h1>Informazioni titolo scelto</h1>
-      <ul>
-        <li v-for="movie in movies" :key="movie.id"><strong>Movie Title - </strong> {{ movie.title }} - <strong>Original Movie Title - </strong> {{ movie.original_title }} <strong>Movie Lenguage - </strong>{{ movie.original_language }} - <strong>Avarage Movie Vote - </strong> {{ movie.vote_average }}</li>
-        <li v-for="serie in series" :key="serie.id"><strong>Serie Title - </strong> {{ serie.name }} - <strong>Original Serie Title - </strong> {{ serie.original_name }} <strong>Serie Lenguage - </strong>{{ serie.original_language }} - <strong>Avarage Serie Vote - </strong> {{ serie.vote_average }}</li>
-      </ul>
+      <div class="container">
+        <h1>Informazioni titolo scelto</h1>
+        <ul>
+          <li v-for="movie in movies" :key="movie.id">
+            <strong>Movie Title - </strong> {{ movie.title }} -
+            <strong>Original Movie Title - </strong> {{ movie.original_title }}
+            <strong>Movie Lenguage - </strong>{{ movie.original_language }}
+            <strong>Avarage Movie Vote - </strong> {{ movie.vote_average }}
+          </li>
+          <li v-for="serie in series" :key="serie.id">
+            <strong>Serie Title - </strong> {{ serie.name }} -
+            <strong>Original Serie Title - </strong> {{ serie.original_name }}
+            <strong>Serie Lenguage - </strong>{{ serie.original_language }}
+            <strong>Avarage Serie Vote - </strong> {{ serie.vote_average }}
+          </li>
+        </ul>
+      </div>
     </main>
     <footer></footer>
   </div>
@@ -31,13 +50,32 @@ export default {
       movies: [],
       series: [],
       keyword: "",
+      apiKey: "6dace570cbb7c25a571ec1d0a83908b1",
     };
   },
   methods: {
-    getQueryMovies() {
+    getQuerys() {
       axios
         .get(
-          `https://api.themoviedb.org/3/search/movie/?query=${this.keyword}&api_key=6dace570cbb7c25a571ec1d0a83908b1`
+          `https://api.themoviedb.org/3/search/tv/?query=${this.keyword}&api_key=${this.apiKey}`
+        )
+        .then((res) => {
+          //estraggo da response i "results" delle serie TV
+          this.series = res.data["results"].map((result) => {
+            const { id, name, original_name, original_language, vote_average } =
+              result;
+            return {
+              id,
+              name,
+              original_name,
+              original_language,
+              vote_average,
+            };
+          });
+        });
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie/?query=${this.keyword}&language=it-IT&api_key=${this.apiKey}`
         )
         .then((res) => {
           //estraggo da response i "results" dei films
@@ -59,38 +97,13 @@ export default {
           });
         });
     },
-    getQuerySeries() {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/search/tv/?query=${this.keyword}&api_key=6dace570cbb7c25a571ec1d0a83908b1`
-        )
-        .then((res) => {
-          //estraggo da response i "results" delle serie TV
-          this.series = res.data["results"].map((result) => {
-            const {
-              id,
-              name,
-              original_name,
-              original_language,
-              vote_average,
-            } = result;
-            return {
-              id,
-              name,
-              original_name,
-              original_language,
-              vote_average,
-            };
-          });
-        });
-    },
   },
   mounted() {
-    this.getQueryMovies();
-    this.getQuerySeries();
+    this.getQuerys();
   },
 };
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap";
 </style>
