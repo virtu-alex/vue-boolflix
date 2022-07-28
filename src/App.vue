37484aba47f526b347326ce1a839e1b16e600fd1
virtu-alex <alessio.cordari@gmail.com>
2022-07-28 15:08:43 +0200

    Base structure + axios call

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseHeader @search-movies="getQueryMovies" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseHeader from "./components/BaseHeader.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BaseHeader,
+  },
+  data() {
+    return {
+      movies: [],
+      querySearch: "",
+    };
+  },
+  methods: {
+    getQueryMovies() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie/?query=${this.querySearch}&api_key=6dace570cbb7c25a571ec1d0a83908b1`
+        )
+        .then((res) => {
+          //estraggo da response i "results"
+          this.movies = res.data["results"];
+          console.log(this.movies);
+        });
+    },
+    mounted(){
+      this.getQueryMovies()
+    }
+  },
+  
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <input placeholder="Nome Film" type="text" />
+    <button @click="$emit('searchMovies')">Search...</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "BaseHeader",
+};
+</script>
+
+<style>
+</style>
